d73b157b87ac3cd76d35e1e677279af3ab1e0618
Kachel Wittig <kjwittig@gmail.com>
2018-03-21 21:02:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |name, season_hash|
-
+    snake_case_to_string(name)
     puts name.to_s.capitalize! + ":"
     season_hash.each do |holiday_sym, holiday_arr|
       puts "  #{holiday_sym.to_s.capitalize}: #{holiday_arr.join ", "}"
