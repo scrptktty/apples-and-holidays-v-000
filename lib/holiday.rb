17c473f675673bd854f753d62a8f0cd95f914390
Kachel Wittig <kjwittig@gmail.com>
2018-03-21 20:06:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, season_hash|
     # season_cap = season.to_s.capitalize + ":"
       season.each do |holiday_sym, holiday_arr|
-        puts season_cap + holiday_sym + holiday_arr
+        puts holiday_sym + holiday_arr
     end
   end
 end
